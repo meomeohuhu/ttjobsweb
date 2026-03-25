a338cdf1830221590c50ef372afea8d424b5a52a
meomeohuhu <phimjvay@gmail.com>
2026-03-25 21:09:58 +0700

    update jwt-token

--- backend/src/main/java/com/ttjobs/backend/controller/UserController.java
+++ backend/src/main/java/com/ttjobs/backend/controller/UserController.java
@@ -3,8 +3,12 @@ package com.ttjobs.backend.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import com.ttjobs.backend.dto.LoginRequest;
 import com.ttjobs.backend.entity.User;
 import com.ttjobs.backend.service.UserService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RestController
 @RequestMapping("/api/users")
@@ -17,5 +21,14 @@ public class UserController {
     public User register(@RequestBody User user){
         return userService.register(user);
     }
+    @PostMapping("/login")
+    public String login(@RequestBody LoginRequest request){
+        return userService.login(request.getEmail(), request.getPassword());
+    }
+    @GetMapping("/api/admin/test")
+    public String adminTest() {
+        return "Admin access OK";
+    }
+    
 
 }
